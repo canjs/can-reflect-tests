a001e85a8ac6b1c54a5b22b0db08243f068853c9
Justin Meyer <justinbmeyer@gmail.com>
2017-11-09 09:47:38 -0600

    require correctly

--- observables/map-like/type/type.js
+++ observables/map-like/type/type.js
@@ -1,6 +1,6 @@
 var defineInstanceKey = require("./define-instance-key");
 var onInstanceBoundChange = require("./on-instance-bound-change");
-var onInstanceBatches = require("./on-instance-bound-patches");
+var onInstanceBatches = require("./on-instance-patches");
 
 module.exports = function(name, makeType) {
     defineInstanceKey(name, makeType);
